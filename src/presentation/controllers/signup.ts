271c97c033367c4f6a74178449808c834bd01a92
KPMGE <jesuskevin254@gmail.com>
2023-01-07 19:34:17 -0300

    :recycle: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,12 @@ import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) return badRequest(new MissingParamError('name'))
-    if (!httpRequest.body.email) return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      } 
+    }
   }
 }
