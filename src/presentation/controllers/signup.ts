85606ab8bae43487144c3951cd6b38c3326f3124
KPMGE <jesuskevin254@gmail.com>
2023-01-07 19:30:50 -0300

    :recycle: add badRequest helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,10 @@
 import { MissingParamError } from "../errors/missing-param-error"
+import { badRequest } from "../helpers/http-helpers"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
-    }
+    if (!httpRequest.body.name) return badRequest(new MissingParamError('name'))
+    if (!httpRequest.body.email) return badRequest(new MissingParamError('email'))
   }
 }
