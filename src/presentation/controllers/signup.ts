1bcd01b1b25c7cea474eb021bd74aa48c2bbb781
KPMGE <jesuskevin254@gmail.com>
2023-01-08 16:27:47 -0300

    :recycle: enhance imports

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
 import { InvalidParamError, MissingParamError } from "../errors"
 import { badRequest, serverError } from "../helpers/http-helpers"
-import { Controller } from "../protocols/controller"
-import { EmailValidator } from "../protocols/email-validator"
-import { HttpRequest, HttpResponse } from "../protocols/http"
+import { 
+  Controller, 
+  EmailValidator, 
+  HttpRequest,
+  HttpResponse 
+} from "../protocols"
 
 export class SignUpController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
