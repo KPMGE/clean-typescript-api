0791dd015a063d1384bd9771e8091dd282c34ab4
KPMGE <jesuskevin254@gmail.com>
2023-01-14 10:22:06 -0300

    :recycle: move db-add-account protocols to a single file

--- src/data/add-account/db-add-account.spec.ts
+++ src/data/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
-import { Encrypter } from "../protocols/encrypter"
 import { DbAddAccount } from './db-add-account'
+import { Encrypter } from "./db-add-account-protocols"
 
 type SutTypes = {
   sut: DbAddAccount,
