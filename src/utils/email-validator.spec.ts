57d2a958dce8d2fd840f911435ab8292754bdda8
KPMGE <jesuskevin254@gmail.com>
2023-01-14 09:13:51 -0300

    :heavy_plus_sign: add validator library

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -6,4 +6,10 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalidmail@mail.com')
     expect(isValid).toBe(false)
   })
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('validmail@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
