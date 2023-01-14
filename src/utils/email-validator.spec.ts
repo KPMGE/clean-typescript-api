245c10686a1575a70aaede324f119027ad2bcc37
KPMGE <jesuskevin254@gmail.com>
2023-01-14 09:22:30 -0300

    :white_check_mark: ensure EmailValidatorAdapter calls isValid method with correct values

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -9,7 +9,6 @@ describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-
     const isValid = sut.isValid('invalidmail@mail.com')
     expect(isValid).toBe(false)
   })
@@ -18,5 +17,12 @@ describe('EmailValidator Adapter', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('validmail@mail.com')
     expect(isValid).toBe(true)
+  })
+
+  it('Should call isValid method with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
