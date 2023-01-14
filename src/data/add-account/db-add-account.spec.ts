b379c92629a8e1bc2afa6dcafc08caa939a5f474
KPMGE <jesuskevin254@gmail.com>
2023-01-14 10:45:13 -0300

    :white_check_mark: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/add-account/db-add-account.spec.ts
+++ src/data/add-account/db-add-account.spec.ts
@@ -78,4 +78,16 @@ describe('DbAddAccount use-case', () => {
       password: 'hashed_password'
     })
   })
+
+  it('should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name', 
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
