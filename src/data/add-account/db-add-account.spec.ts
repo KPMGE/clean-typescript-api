bbb85dc002a18aa62386796b28775fcba1f3681f
KPMGE <jesuskevin254@gmail.com>
2023-01-14 10:09:00 -0300

    :recycle: move sut creation to a factory helper method

--- src/data/add-account/db-add-account.spec.ts
+++ src/data/add-account/db-add-account.spec.ts
@@ -1,16 +1,27 @@
 import { Encrypter } from "../protocols/encrypter"
 import { DbAddAccount } from './db-add-account'
 
+type SutTypes = {
+  sut: DbAddAccount,
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(text: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return { sut, encrypterStub }
+}
+
 describe('DbAddAccount use-case', () => {
   it('should call encrypter with password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt(text: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encrypterSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name', 
