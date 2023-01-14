c88204e4541fc8248e900a852a589cd40560bd9e
KPMGE <jesuskevin254@gmail.com>
2023-01-14 08:51:18 -0300

    :recycle: log error on SignupContoller

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -41,6 +41,7 @@ export class SignUpController implements Controller {
 
       return ok(account)
     } catch (error) {
+      console.error('[ERROR]: ', error)
       return serverError()
     }
   }
