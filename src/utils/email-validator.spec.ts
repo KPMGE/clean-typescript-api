c7b1637caf7fcc2942c3bd1eecfcba90eb43980b
KPMGE <jesuskevin254@gmail.com>
2023-01-14 09:18:04 -0300

    :white_check_mark: ensure EmailValidator works with library integration

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,8 +1,15 @@
 import { EmailValidatorAdapter } from "./email-validator"
+import validator from "validator"
+
+jest.mock('validator', () => ({
+  isEmail(): boolean { return true }
+}))
 
 describe('EmailValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
     const isValid = sut.isValid('invalidmail@mail.com')
     expect(isValid).toBe(false)
   })
