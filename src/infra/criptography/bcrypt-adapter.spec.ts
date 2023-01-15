bc896fe027436d85fc3e20ab38f7392e7e4db1c5
KPMGE <jesuskevin254@gmail.com>
2023-01-14 21:28:13 -0300

    :recycle: move sut creation to a factory helper method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
@@ -7,18 +8,22 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+const makeSut = (): Encrypter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+
 describe('BcryptAdapter', () => {
   it('should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('some hash')
   })
