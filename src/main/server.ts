1817f8055adbebd16c245f525e1bd81454f2f8d4
KPMGE <jesuskevin254@gmail.com>
2023-01-15 15:12:02 -0300

    :white_check_mark: ensure bodyParser middleware parses request body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express'
-
-const app = express()
+import { app } from "./config/app"
 
 const port = 5050
 app.listen(port, () => console.log(`server running at: http://localhost:${port}`))
