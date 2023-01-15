96483d6547ad8b94344acc0d1334b55ff6b53db1
KPMGE <jesuskevin254@gmail.com>
2023-01-15 15:30:43 -0300

    :white_check_mark: ensure content-type middleware sets headers correctly

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import { app } from '../config/app'
+
+describe('Content Type Middleware', () => {
+  it('should set content type header to json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  it('should set content type to xml when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
