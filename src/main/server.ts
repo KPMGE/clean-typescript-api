843aded53af0efa64ff90cef00c8ac9b6194c6f9
KPMGE <jesuskevin254@gmail.com>
2023-01-16 07:59:29 -0300

    :recycle: enhance server.ts readability

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,10 @@
+import { env } from "./config/env"
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper"
 import { app } from "./config/app"
 
-const port = 5050
-app.listen(port, () => console.log(`server running at: http://localhost:${port}`))
+MongoHelper.connect(env.mongUri)
+  .then(() => {
+    console.log('mongodb connected!')
+    app.listen(env.port, () => console.log(`server running at: http://localhost:${env.port}`))
+  })
+  .catch(err => console.error('[ERROR]: ', err))
